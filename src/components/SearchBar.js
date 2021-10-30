5e49afa79b4ad225c7d46975f4c808e6808bf7b2
CH Usman <44199426+usmanchaudary@users.noreply.github.com>
2021-10-30 18:24:52 +0500

    working on add todo

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,58 @@
-import { Row, Col,InputGroup, FormControl } from 'react-bootstrap'
+import { Row, Col, InputGroup, FormControl } from 'react-bootstrap'
 import $ from 'jquery'
-
+import { useState } from 'react';
+import '../App.css'
 
 function SearchBar() {
-    function AppendKey(e) {
-        e.preventDefault();
-        var value = $('#searchBar').val();
+    const [todos, setTodos] = useState([
+        {text: 'i am first'},
+        {text: 'i am second'},
 
-    }
+    ]);
+
+    const [value,setValue] = useState("");
 
     return (
         <>
             <Row>
-            <Col md={6}>
-                <InputGroup className="col-md-6 mb-3" style={{marginLeft:460, marginTop:50}}>
-                    <FormControl
-                        placeholder="search here . . ."
-                        aria-label="searchBox"
-                        aria-describedby="basic-addon1" 
-                        onKeyUp={AppendKey}
-                        id="searchBar"
-                    />
-                    <InputGroup.Text id="basic-addon1" onClick={()=>alert('hello')}> @</InputGroup.Text>
+                <Col md={6}>
+                    <InputGroup className="col-md-6 mb-3" style={{ marginLeft: 460, marginTop: 50 }}>
+                        <FormControl
+                            placeholder="search here . . ."
+                            aria-label="searchBox"
+                            aria-describedby="basic-addon1"
+                            value={value}
+                            onChange={e=>{setValue(e.target.value); } }
+                            id="searchBar"
+                        />
+                        <InputGroup.Text id="basic-addon1" onClick={() => alert('hello')}> @</InputGroup.Text>
 
-                </InputGroup>
+                    </InputGroup>
                 </Col>
             </Row>
+
+            <div className="app">
+                <div className="todo-list">
+                    {todos.map((todo, index) => (
+                        <Todo
+                            index={index}
+                            todo={todo}
+                        />
+                    ))}
+                </div>
+            </div>
+
+
         </>
+    );
+}
+
+function Todo({ todo,index }) {
+    return (
+        <div className="todo">
+            {todo.text}
+        </div>
+
     );
 }
 export default SearchBar;
